85f53b6a43d1b78db8ab5f3474a1fffc3092aa95
stephane <stephaneagg@gmail.com>
2026-02-28 21:15:18 -0800

    implemented JwtService as a piece of the JwtAuthenticationFilter work. Added a few dependencies for this aswell

--- src/main/java/com/steph/user/UserRepository.java
+++ src/main/java/com/steph/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.steph.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
