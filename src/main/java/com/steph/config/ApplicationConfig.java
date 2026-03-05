9f10a26500bfbf087924ee058ca1ae98b4c2281d
stephane <stephaneagg@gmail.com>
2026-03-04 21:20:53 -0800

    Fixed issue with userDetailsService called wrong findBy method to find user. Added correct method and fixed call. This should complete the authorisation implementation

--- src/main/java/com/steph/config/ApplicationConfig.java
+++ src/main/java/com/steph/config/ApplicationConfig.java
@@ -26,7 +26,7 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         // fetch user from database
-        return username -> userRepository.findByEmail(username)
+        return username -> userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
