d98625c696cd3708c46532a8adb7241263ce3630
Stephane <stephaneagg@gmail.com>
2026-04-04 21:07:57 -0700

    fixed import issues preventing build

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import Feed from "./pages/Feed.jsx"
 
-import './App.css'
+import './style/App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </BrowserRouter>
   )
