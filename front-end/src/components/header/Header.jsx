2c5254270e52374144fcca072683d24d124d9d8b
stephane <stephaneagg@gmail.com>
2026-04-17 18:24:05 -0700

    implemented and styled the header component. Buttons and search are non functional

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -1,11 +1,41 @@
 import "./header.scss"
+import HomeIcon from '@mui/icons-material/Home';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import AppsIcon from '@mui/icons-material/Apps';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
+        <div className="header">
+            <div className="left">
+                <Link to="/" style={{textDecoration:"none"}}>
+                <span>Steph's Social</span>
+                </Link>
+                <HomeIcon />
+                <DarkModeIcon />
+                <AppsIcon />
 
-        <>
-        Header
-        </>
+                <div className="search">
+                    <SearchIcon />
+                    <input type="text" placeholder="Search..."/>
+                </div>
 
+            </div>
+
+            <div className="right">
+                <PersonIcon />
+                <NotificationsIcon />
+                <div className="user">
+                    <img src="src/resources/tempProfileIcon.jpeg" alt=""/>
+                    <span>Jane Doe</span>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
