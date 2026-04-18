176da7b43062c5c6c110b0963f6e7fd44d4e2e82
stephane <stephaneagg@gmail.com>
2026-04-17 18:24:57 -0700

    implemented and styled the leftbar component buttons are non-functional

--- front-end/src/components/leftBar/LeftBar.jsx
+++ front-end/src/components/leftBar/LeftBar.jsx
@@ -3,8 +3,44 @@ import "./leftBar.scss"
 
 export default function LeftBar() {
   return (
-    <div>
-      LeftBar
+    <div className="leftBar">
+
+      <div className="container">
+
+        <div className="menu">
+
+          <div className="user">
+            <img src="src/resources/profileIcon.png" alt="" />
+            <span>Jane Doe</span>
+          </div>
+
+          <div className="item">
+            <img src="src/resources/followers.png" alt=""/>
+            <span>Friends</span>
+          </div>
+
+          <div className="item">
+            <img src="src/resources/groupIcon.png" alt=""/>
+            <span>Groups</span>
+          </div>
+
+          <div className="item">
+            <img src="src/resources/messageIcon.png" alt=""/>
+            <span>Messages</span>
+          </div>
+
+          <div className="item">
+            <img src="src/resources/galleryIcon.png" alt=""/>
+            <span>Gallery</span>
+          </div>
+
+          <div className="item">
+            <img src="src/resources/gameIcon.png" alt=""/>
+            <span>Gaming</span>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
