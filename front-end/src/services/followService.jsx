7688b269dde129deba1347d8f53784045c3f8d99
stephane <stephaneagg@gmail.com>
2026-04-28 21:56:37 -0700

    implemented api calls for followService functions

--- front-end/src/services/followService.jsx
+++ front-end/src/services/followService.jsx
@@ -3,18 +3,61 @@ const API_URL = "http://localhost:8080/follows/"
 
 export async function getFollowers(userId) {
   const token = localStorage.getItem()
-}
 
+  const res = await fetch(`${API_URL}/followers/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch followers");
+  }
+}
 
 export async function getFollowees(userId) {
   const token = localStorage.getItem()
+
+  const res = await fetch(`${API_URL}/following/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch followees");
+  }
 }
 
 export async function follow(userId) {
   const token = localStorage.getItem()
+
+  const res = await fetch(`${API_URL}/${userId}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to follow userId: ${userId}`);
+  }
 }
 
 export async function unfollow(userId) {
   const token = localStorage.getItem()
+
+  const res = await fetch(`${API_URL}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to unfollow userId: ${userId}`);
+  }
 }
 
