1e13f82f44d8e3f3b94d0817922bacb9d484f638
stephane <stephaneagg@gmail.com>
2026-05-01 15:32:10 -0700

    implemented API call function for feedService

---
+++ front-end/src/services/feedService.jsx
@@ -0,0 +1,22 @@
+const API_URL = "http://localhost:8080/feed"
+
+export async function getFeed(){
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(`${API_URL}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  if (!res.ok) {
+    const data = await res.text().then(t => {
+      try { return JSON.parse(t); } catch { return t; }
+    });
+
+    throw new Error(data.message || data);
+  }
+
+  return await res.json() // [ {{id, authorId, authorName, content, imageUrl, createdAt}}, ... ]
+}
