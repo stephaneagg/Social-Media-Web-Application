f1bff39299818d975ae5968745c05d1e67c5a9aa
stephane <stephaneagg@gmail.com>
2026-05-23 18:42:41 -0700

    wrote script to insert posts

---
+++ back-end/database/seed_posts.sql
@@ -0,0 +1,97 @@
+INSERT INTO posts (
+    user_id,
+    created_at,
+    image_url,
+    content_text
+)
+VALUES
+    (
+        1,
+        '2026-04-27 17:37:53.379 -0700',
+        'back-end/uploads/posts/5cd611a7-52bb-4606-ae7b-ac99c8aa5197.jpg',
+        'First post ever. Snowy Elk. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
+    ),
+    (
+        1,
+        '2026-04-28 17:37:53.379 -0700',
+        null,
+        'Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.'
+    ),
+    (
+        1,
+        '2026-04-29 17:37:53.379 -0700',
+        null,
+        'Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.'
+    ),
+    (
+        1,
+        '2026-04-29 17:37:53.379 -0700',
+        'back-end/uploads/posts/5cd611a7-52bb-4606-ae7b-ac99c8aa5197.jpg',
+        'First post ever. Snowy Elk. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
+    ),
+    (
+        2,
+        '2026-04-28 17:37:53.379 -0700',
+        null,
+        'Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.'
+    ),
+    (
+        2,
+        '2026-04-28 17:38:53.379 -0700',
+        null,
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.'
+    ),
+    (
+        2,
+        '2026-04-28 17:39:53.379 -0700',
+        'back-end/uploads/posts/60f14068-56bf-4bc0-8944-d87d87e46990.jpg',
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.'
+    ),
+    (
+        3,
+        '2026-04-28 18:38:53.379 -0700',
+        'back-end/uploads/posts/36170ef0-4c71-4b19-990b-1ab410defd68.jpg',
+        'Ex nihil dolorum hic asperiores adipisci et laudantium maiores et vero quidem quo dolor facere vel molestias voluptatem et maxime minima? Eos dolorem quos eos accusamus iste ut omnis dolorum sed fuga quisquam eos voluptates laboriosam.'
+    ),
+    (
+        3,
+        '2026-04-28 19:38:53.379 -0700',
+        'back-end/uploads/posts/744bd923-0161-405c-a43b-ab473153bba6.jpg',
+        'Qui nihil tempore cum reprehenderit molestiae non commodi ipsum sed dolore odio ab assumenda cupiditate?'
+    ),
+    (
+        3,
+        '2026-04-28 20:38:53.379 -0700',
+        null,
+        'Quo nemo ipsum ut accusantium provident aut optio quisquam qui voluptate molestiae ut vero consequatur.'
+    ),
+    (
+        3,
+        '2026-04-28 21:38:53.379 -0700',
+        null,
+        'Hic illum laborum non iusto dolores et maiores quia. Et vero minima qui fugit fuga aut illo similique et molestiae fugit non excepturi suscipit.'
+    ),
+    (
+        4,
+        '2026-04-29 20:38:53.379 -0700',
+        null,
+        'Eos animi ullam nam doloremque excepturi est fugiat quia. Qui officia corporis in expedita commodi sed commodi reprehenderit ut iusto porro?'
+    ),
+    (
+        4,
+        '2026-04-29 20:38:53.379 -0700',
+        null,
+        'In aperiam laudantium et voluptate quia aut doloremque ipsa. Quo laudantium placeat At perferendis voluptatibus quo omnis voluptatem. Eum quasi dignissimos ad placeat sunt est possimus ullam qui ipsum doloremque est delectus architecto? Ut dolorum esse id quia necessitatibus est provident eius aut soluta Quis qui consequatur minus et ullam consequatur aut quod libero.'
+    ),
+    (
+        4,
+        '2026-04-29 20:38:53.379 -0700',
+        'back-end/uploads/posts/e5884489-f7dd-4353-95ca-19e4ac084fcd.jpg',
+        'Sit facilis autem ut deleniti unde ut nulla nemo ut unde quod sit voluptatem itaque eum voluptas voluptate. Aut facere soluta At nemo minima sit similique porro. 33 omnis nemo At necessitatibus aspernatur id debitis dolorem.'
+    ),
+    (
+        4,
+        '2026-04-29 20:38:53.379 -0700',
+        'back-end/uploads/posts/a6b35de7-5588-4fb8-a630-2891bc1fc1b2.png',
+        'Ut laboriosam enim est harum iusto aut modi tempora vel internos quis sed cupiditate vitae 33 nihil rerum. Qui explicabo sint qui galisum repellendus est omnis delectus sit quasi aperiam. At voluptate vero eos nostrum itaque eum totam numquam ut rerum expedita ut quas veniam.'
+    )
