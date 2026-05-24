c7724ac908e7bc634fc0b7cd38a2f68f75a97508
stephane <stephaneagg@gmail.com>
2026-05-23 19:28:17 -0700

    wrote script to insert comment data

---
+++ back-end/database/seed_comments.sql
@@ -0,0 +1,367 @@
+INSERT INTO comments (
+    post_id,
+    user_id,
+    created_at,
+    content
+)
+VALUES
+    (
+         1,
+         2,
+         '2026-04-30 17:37:53.379 -0700',
+         'First!'
+    ),
+    (
+        1,
+        3,
+        '2026-04-30 17:38:53.379 -0700',
+        'Eos vero nihil ut amet error eos magni nobis.'
+    ),
+    (
+        1,
+        4,
+        '2026-04-30 17:39:53.379 -0700',
+        'Vel porro inventore sit velit fuga est perspiciatis blanditiis non enim consequatur et distinctio delectus.'
+    ),
+    (
+        1,
+        5,
+        '2026-04-30 17:39:53.379 -0700',
+        'In enim quia sed quaerat quia est quibusdam error.'
+    ),
+    (
+        2,
+        1,
+        '2026-04-30 17:39:53.379 -0700',
+        'Aut recusandae atque in veritatis quia sit perspiciatis quisquam ut asperiores accusamus est voluptas tenetur'
+    ),
+    (
+        2,
+        2,
+        '2026-04-30 17:39:53.379 -0700',
+        'Hic similique libero aut autem assumenda vel maxime vitae'
+    ),
+    (
+        2,
+        3,
+        '2026-04-30 17:39:53.379 -0700',
+        'Qui provident quae qui suscipit sunt 33 dolor repellendus ea voluptatibus minima!'
+    ),
+    (
+        2,
+        4,
+        '2026-04-30 17:39:53.379 -0700',
+        'Sed maxime quidem et accusantium ullam hic esse soluta et excepturi minus!'
+    ),
+    (
+        2,
+        5,
+        '2026-04-30 17:39:53.379 -0700',
+        'Quo neque nulla aut voluptas galisum quo voluptatem fugiat'
+    ),
+    (
+        3,
+        2,
+        '2026-04-30 18:39:53.379 -0700',
+        'Hic similique libero aut autem assumenda vel maxime vitae'
+    ),
+    (
+        3,
+        3,
+        '2026-04-30 18:32:53.379 -0700',
+        'Qui provident quae qui suscipit sunt 33 dolor repellendus ea voluptatibus minima!'
+    ),
+    (
+        3,
+        4,
+        '2026-04-30 18:33:53.379 -0700',
+        'Sed maxime quidem et accusantium ullam hic esse soluta et excepturi minus!'
+    ),
+    (
+        3,
+        5,
+        '2026-04-30 18:33:53.379 -0700',
+        'Quo neque nulla aut voluptas galisum quo voluptatem fugiat'
+    ),
+    (
+        4,
+        2,
+        '2026-04-30 17:39:53.379 -0700',
+        'Hic similique libero aut autem assumenda vel maxime vitae'
+    ),
+    (
+        4,
+        3,
+        '2026-04-30 17:40:53.379 -0700',
+        'Qui provident quae qui suscipit sunt 33 dolor repellendus ea voluptatibus minima!'
+    ),
+    (
+        4,
+        4,
+        '2026-04-30 17:41:53.379 -0700',
+        'Sed maxime quidem et accusantium ullam hic esse soluta et excepturi minus!'
+    ),
+    (
+        4,
+        5,
+        '2026-04-30 17:42:53.379 -0700',
+        'Quo neque nulla aut voluptas galisum quo voluptatem fugiat'
+    ),
+    (
+        5,
+        1,
+        '2026-04-30 15:42:53.379 -0700',
+        'In galisum officia vel fugiat magni At neque aspernatur eum soluta nihil est consequuntur veritatis'
+    ),
+    (
+        5,
+        3,
+        '2026-04-30 15:43:53.379 -0700',
+        'Eum atque adipisci eum atque neque et quae quia quo enim autem qui voluptas placeat sit saepe quos sed nobis sapiente'
+    ),
+    (
+        5,
+        4,
+        '2026-04-30 15:44:53.379 -0700',
+        'Et mollitia enim et galisum animi ut adipisci quibusdam'
+    ),
+    (
+        5,
+        4,
+        '2026-04-30 15:44:53.379 -0700',
+        'Vel ipsum ducimus hic dolorem modi qui minus delectus qui omnis accusamus ea minima voluptas'
+    ),
+    (
+        6,
+        1,
+        '2026-04-30 15:42:53.379 -0700',
+        'In galisum officia vel fugiat magni At neque aspernatur eum soluta nihil est consequuntur veritatis'
+    ),
+    (
+        6,
+        3,
+        '2026-04-30 15:43:53.379 -0700',
+        'Eum atque adipisci eum atque neque et quae quia quo enim autem qui voluptas placeat sit saepe quos sed nobis sapiente'
+    ),
+    (
+        6,
+        3,
+        '2026-04-30 15:44:53.379 -0700',
+        'Et mollitia enim et galisum animi ut adipisci quibusdam'
+    ),
+    (
+        6,
+        4,
+        '2026-04-30 15:44:53.379 -0700',
+        'Vel ipsum ducimus hic dolorem modi qui minus delectus qui omnis accusamus ea minima voluptas'
+    ),
+    (
+        7,
+        1,
+        '2026-04-30 15:42:53.379 -0700',
+        'In galisum officia vel fugiat magni At neque aspernatur eum soluta nihil est consequuntur veritatis'
+    ),
+    (
+        7,
+        1,
+        '2026-04-30 15:43:53.379 -0700',
+        'Eum atque adipisci eum atque neque et quae quia quo enim autem qui voluptas placeat sit saepe quos sed nobis sapiente'
+    ),
+    (
+        7,
+        3,
+        '2026-04-30 15:44:53.379 -0700',
+        'Et mollitia enim et galisum animi ut adipisci quibusdam'
+    ),
+    (
+        7,
+        4,
+        '2026-04-30 15:44:53.379 -0700',
+        'Vel ipsum ducimus hic dolorem modi qui minus delectus qui omnis accusamus ea minima voluptas'
+    ),
+    (
+        8,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'Ut adipisci aliquam est excepturi minus rem corrupti explicabo'
+    ),
+    (
+        8,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eum atque adipisci eum atque neque et quae quia quo enim autem qui voluptas placeat sit saepe quos sed nobis sapiente'
+    ),
+    (
+        8,
+        2,
+        '2026-05-02 15:44:53.379 -0700',
+        'Et mollitia enim et galisum animi ut adipisci quibusdam'
+    ),
+    (
+        8,
+        4,
+        '2026-05-02 15:44:53.379 -0700',
+        'Eum mollitia sapiente in eius quia sit quod voluptatum'
+    ),
+    (
+        9,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'Ut adipisci aliquam est excepturi minus rem corrupti explicabo'
+    ),
+    (
+        9,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eum atque adipisci eum atque neque et quae quia quo enim autem qui voluptas placeat sit saepe quos sed nobis sapiente'
+    ),
+    (
+        9,
+        4,
+        '2026-05-02 15:44:53.379 -0700',
+        'Et mollitia enim et galisum animi ut adipisci quibusdam'
+    ),
+    (
+        9,
+        4,
+        '2026-05-02 15:45:53.379 -0700',
+        'Eum mollitia sapiente in eius quia sit quod voluptatum'
+    ),
+    (
+        10,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'Eum accusantium quia ut dicta ipsum et suscipit odio vel dolore voluptatem ex ipsam doloribus qui nulla fuga!'
+    ),
+    (
+        10,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eum atque adipisci eum atque neque et quae quia quo enim autem qui voluptas placeat sit saepe quos sed nobis sapiente'
+    ),
+    (
+        10,
+        4,
+        '2026-05-02 15:44:53.379 -0700',
+        'Id quia vero ut nihil autem qui optio nemo a sequi blanditiis'
+    ),
+    (
+        10,
+        4,
+        '2026-05-02 15:45:53.379 -0700',
+        'Ad assumenda quia in quas deleniti quo labore ipsum'
+    ),
+    (
+        11,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'Et soluta repellendus rem voluptatem sapiente quo laudantium ducimus'
+    ),
+    (
+        11,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eos accusantium corrupti et nisi repellat et internos eaque'
+    ),
+    (
+        11,
+        4,
+        '2026-05-02 15:44:53.379 -0700',
+        'At inventore distinctio id doloremque corporis.'
+    ),
+    (
+        11,
+        4,
+        '2026-05-02 15:45:53.379 -0700',
+        'Sed iure voluptatem ut voluptatem corrupti est sunt aliquam'
+    ),
+    (
+        12,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'Et soluta repellendus rem voluptatem sapiente quo laudantium ducimus'
+    ),
+    (
+        12,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eos accusantium corrupti et nisi repellat et internos eaque'
+    ),
+    (
+        12,
+        3,
+        '2026-05-02 15:44:53.379 -0700',
+        'At inventore distinctio id doloremque corporis.'
+    ),
+    (
+        12,
+        3,
+        '2026-05-02 15:45:53.379 -0700',
+        'Sed iure voluptatem ut voluptatem corrupti est sunt aliquam'
+    ),
+    (
+        13,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'd deleniti cumque aut consequatur inventore qui possimus voluptas et laudantium delectus'
+    ),
+    (
+        13,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eos accusantium corrupti et nisi repellat et internos eaque'
+    ),
+    (
+        13,
+        3,
+        '2026-05-02 15:44:53.379 -0700',
+        'Ut enim doloribus eos modi neque et porro esse ea voluptatem recusandae aut voluptatibus omnis!'
+    ),
+    (
+        13,
+        3,
+        '2026-05-02 15:45:53.379 -0700',
+        'Sed iure voluptatem ut voluptatem corrupti est sunt aliquam'
+    ),
+    (
+        14,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'd deleniti cumque aut consequatur inventore qui possimus voluptas et laudantium delectus'
+    ),
+    (
+        14,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'Eos accusantium corrupti et nisi repellat et internos eaque'
+    ),
+    (
+        14,
+        2,
+        '2026-05-02 15:44:53.379 -0700',
+        'Ut enim doloribus eos modi neque et porro esse ea voluptatem recusandae aut voluptatibus omnis!'
+    ),
+    (
+        15,
+        1,
+        '2026-05-02 15:42:53.379 -0700',
+        'consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua'
+    ),
+    (
+        15,
+        2,
+        '2026-05-02 15:43:53.379 -0700',
+        'nisi ut aliquid ex ea commodi consequatur'
+    ),
+    (
+        15,
+        2,
+        '2026-05-02 15:44:53.379 -0700',
+        'Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+    ),
+    (
+        15,
+        3,
+        '2026-05-02 15:45:53.379 -0700',
+        'Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    );
