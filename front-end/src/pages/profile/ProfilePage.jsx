ff5c36b31025b3634ebc825876d826d34fabff63
stephane <stephaneagg@gmail.com>
2026-05-29 18:57:05 -0700

    updated the look of the user info in the profile page

--- front-end/src/pages/profile/ProfilePage.jsx
+++ front-end/src/pages/profile/ProfilePage.jsx
@@ -2,7 +2,20 @@ import "./profile.scss"
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import Feed from "../../components/feed/Feed.jsx"
 
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext.jsx";
+
+
 export default function ProfilePage() {
+
+  const { id } = useParams();
+  const { currentUser } = useContext(AuthContext)
+  const isOwnProfile = currentUser.id == id;
+  console.log(currentUser.id)
+  console.log(id)
+  console.log(isOwnProfile)
+
   return (
     <div className="profile">
       <div className="images">
@@ -12,26 +25,39 @@ export default function ProfilePage() {
 
       <div className="profileContainer">
         <div className="uInfo">
-          <div className="left">
-            <div className="post-count">
-              <span>3</span>
-              <span>posts</span>
+
+          <div className="topName">
+            <span>{id}</span>
+          </div>
+
+          <div className="middle">
+            <div className="left">
+              <div className="post-count">
+                <span>3</span>
+                <span>posts</span>
+              </div>
+              <div className="follower-count">
+                <span>223</span>
+                <span>followers</span>
+              </div>
+              <div className="following-count">
+                <span>510</span>
+                <span>following</span>
+              </div>
             </div>
-            <div className="follower-count">
-              <span>223</span>
-              <span>followers</span>
-            </div>
-            <div className="following-count">
-              <span>510</span>
-              <span>following</span>
+
+            <div className="right">
+              <MoreHorizIcon />
             </div>
           </div>
+
+
           <div className="center">
-            <button>follow</button>
+            {isOwnProfile ? null : <button>Follow</button>}
           </div>
-          <div className="right">
-            <MoreHorizIcon />
-          </div>
+
+
+
         </div>
         <Feed />
       </div>
