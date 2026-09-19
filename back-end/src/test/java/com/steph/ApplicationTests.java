f946aa496317b015c4eb6fab82d48a297e2a96d9
stephane <stephaneagg@gmail.com>
2026-09-19 12:09:42 -0700

    added integration tests to test suite

--- back-end/src/test/java/com/steph/ApplicationTests.java
+++ back-end/src/test/java/com/steph/ApplicationTests.java
@@ -2,12 +2,35 @@ package com.steph;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(properties = {
+        "JWT_SECRET=dGVzdC1zZWNyZXQta2V5LWZvci10ZXN0aW5n",
+        "JWT_EXPIRATION=86400000",
+        "JWT_REFRESH_EXPIRATION=604800000"
+})
 class ApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:18")
+                    .withDatabaseName("pgdev")
+                    .withUsername("stephg")
+                    .withPassword("password");
+
+    @DynamicPropertySource
+    static void configureDatabase(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
